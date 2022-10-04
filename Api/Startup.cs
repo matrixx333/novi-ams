ba533ee0158d9c0d6f9bc0788c428e3c0f438115
matrixx333 <matrixx333@gmail.com>
2022-10-04 17:15:53 -0400

    updated cors

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -60,7 +60,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost"));
+            app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost", "http://web"));
 
             app.UseAuthentication();
             app.UseAuthorization();
